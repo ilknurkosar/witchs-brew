ca97c7e27fd1ea0300d4222fc8a6e35f32a14648
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-01 15:04:11 +0300

    InputHandle and EventQueue changes
    Co-Authored-By: ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>

--- src/modules/Core/InputHandle/include/InputHandle.hpp
+++ src/modules/Core/InputHandle/include/InputHandle.hpp
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <map>
+#include <functional>
+#include "raylib.h"
+
+enum class EventType {
+    CLICK,
+    KEY_PRESS
+};
+
+struct Event {
+    EventType type;
+};
+
+class InputHandle {
+private:
+    std::map<int, EventType> keyMap;
+    std::function<void(Event)> eventCallback;
+
+public:
+    InputHandle();
+
+    void setEventCallback(std::function<void(Event)> callback);
+
+    void processInput();
+
+    void processMouseInput();
+
+    void processKeyboardInput();
+
+    void handleEvent(EventType eventType);
+
+    void mapKey(int keyCode, EventType eventType);
+
+    void unmapKey(int keyCode);
+
+    void clearKeyMappings();
+
+};
+
+
