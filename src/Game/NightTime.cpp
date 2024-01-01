fb598fab4b4b3a712589bde7ffd3dcee3b6f8493
Dilara <dilaraykut@icloud.com>
2024-01-01 14:45:00 +0000

    Stub implementation of NightTime

---
+++ src/Game/NightTime.cpp
@@ -0,0 +1,4 @@
+#include "NightTime.hpp"
+NightTime::NightTime(){
+    
+}
--- src/Game/include/NightTime.hpp
+++ src/Game/include/NightTime.hpp
@@ -1,1 +1,8 @@
 #pragma once
+#include "Node.hpp"
+#include "RunnableNode.hpp"
+
+class NightTime: public Node, public RunnableNode {
+    public:
+    NightTime();
+};
