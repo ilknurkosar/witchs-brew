a014457b12d744039c9a3fd4150b7d16970bf6d1
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-14 17:37:38 +0300

    Update InputHandle.hpp

--- src/modules/Core/InputHandle/include/InputHandle.hpp
+++ src/modules/Core/InputHandle/include/InputHandle.hpp
@@ -5,8 +5,9 @@
 #include "Runnable.hpp"
 #include "Event.hpp"
 
-namespace InputEvent{
-    enum ENUM{
+// Namespace for input-related events
+namespace InputEvent {
+    enum ENUM {
         KEY_UP,
         KEY_DOWN,
         KEY_RIGHT,
@@ -18,23 +19,31 @@ namespace InputEvent{
     };
 };
 
+// InputHandle class, derived from the Runnable interface
 class InputHandle : public Runnable {
 private:
     static InputHandle* singleton;
-    std::map<int, Event> keyMap;
+    std::map<int, Event> keyMap; // Map to store key mappings
 
 public:
+    // Constructor
     InputHandle();
+
+    // Destructor
     ~InputHandle();
 
+    // Implementation of the process function from the Runnable interface
     virtual void process() override;
 
+    // Static method to map a key to an input event
     static void mapKey(int keyCode, InputEvent::ENUM eventType);
 
+    // Static method to unmap a key
     static void unmapKey(int keyCode);
 
+    // Static method to clear all key mappings
     static void clearKeyMappings();
-
 };
+
 
 
