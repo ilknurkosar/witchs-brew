9f0332d7ab126e7d4f2c2b88c0bd7a163496a62e
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-14 17:36:15 +0300

    Update EventQueue.hpp

--- src/modules/Core/EventQueue/include/EventQueue.hpp
+++ src/modules/Core/EventQueue/include/EventQueue.hpp
@@ -7,7 +7,7 @@
 #include <map>
 #include "Event.hpp"
 
-
+// Constructor
 class EventQueue : public Runnable {
 private:
     static EventQueue* singleton;
@@ -16,14 +16,24 @@ private:
     std::map<Event, std::vector<EventHandler>> eventHandlers;
     std::vector<std::pair<Event, void*>> activeEvents;
 
-public:
+public// Constructor
     EventQueue();
-    ~EventQueue();
 
-    static inline EventQueue* getSingleton(){return singleton;}
-    static inline void fireEvent(Event event, void* data){singleton->activeEvents.push_back({event,data});}
-    static void addHandler(Event event, EventHandler handler); //events are added only after they get a corresponding handler
+    // Destructor
+    ~EventQueue();
+ // Getter for the singleton instance
+    static inline EventQueue* getSingleton() { return singleton; }
+
+    // Static method to fire an event
+    static inline void fireEvent(Event event, void* data) { singleton->activeEvents.push_back({event, data}); }
+
+    // Static method to add an event handler
+    static void addHandler(Event event, EventHandler handler); // Events are added only after they get a corresponding handler
+
+    // Implementation of the process function from the Runnable interface
     void process() override;
+
 private:
+    // Private method to add an event to the active events list
     void addEvent(Event e);
 };
