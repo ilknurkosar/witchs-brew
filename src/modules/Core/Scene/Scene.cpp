edc795ff5812e1099ee28c7eb308ac515d83f1c3
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 19:57:50 +0300

    Doxygen comments of Scene.cpp

--- src/modules/Core/Scene/Scene.cpp
+++ src/modules/Core/Scene/Scene.cpp
@@ -9,8 +9,14 @@
 
 //TODO: Write tests for this class
 
+/**
+ * @brief The singleton instance of the Scene class.
+ */
 Scene *Scene::singleton=nullptr;
 
+/**
+ * @brief Processes the scene by fetching visibles and running runnables.
+ */
 void Scene::process(){
     RendererDefault::fetchVisibles();
     auto runnables = getRunnable();
@@ -19,6 +25,10 @@ void Scene::process(){
     }
 }
 
+/**
+ * @brief Retrieves visual nodes in the scene.
+ * @return Vector of visual nodes.
+ */
 std::vector<Node*> Scene::getVisual(){
     std::vector<Node*> out{};
 
@@ -35,6 +45,11 @@ std::vector<Node*> Scene::getVisual(){
     recurseChild(Scene::getRoot());
     return out;
 }
+
+/**
+ * @brief Gets a vector containing runnable nodes in the scene.
+ * @return Vector of runnable nodes.
+ */
 std::vector<Node*> Scene::getRunnable(){
     std::vector<Node*> out{};
 
@@ -51,22 +66,36 @@ std::vector<Node*> Scene::getRunnable(){
     recurseChild(Scene::getRoot());
     return out;
 }
-
+/**
+ * @brief Default constructor for the Scene class.
+ */
 Scene::Scene()
 : root("root")
 {
     singleton = this;
 }
+
+/**
+ * @brief Default destructor for the Scene class.
+ */
 Scene::~Scene(){
     singleton = nullptr;
 }
 
+/**
+ * @brief Adds a node to the scene.
+ * @param n The node to be added.
+ */
 void Scene::addNode(Node *n){
     // if(n == nullptr) //intentionally removed safeguard
     //     return;
     n->setParent(Scene::getRoot());
 }
 
+/**
+ * @brief Gets the root node of the scene.
+ * @return The root node of the scene.
+ */
 Node* Scene::getRoot(){
     return &singleton->root;
 }
