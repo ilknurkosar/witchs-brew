1ec7e0859e8518d6d69e8a79f6aa284956f86274
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 20:43:20 +0300

    Doxygen comments of Renderer.cpp

--- src/modules/Core/Renderer/Renderer.cpp
+++ src/modules/Core/Renderer/Renderer.cpp
@@ -2,13 +2,24 @@
 
 Renderer *Renderer::singleton = nullptr;
 
+/**
+ * @brief Get the singleton instance of the Renderer class.
+ * @return The singleton instance of the Renderer class.
+ */
 Renderer* Renderer::getSingleton(){
     return singleton;
 }
-
+/**
+ * @brief Constructor for the Renderer class.
+ * @details Initializes the singleton instance.
+ */
 Renderer::Renderer(){
     singleton = this;
 }
+/**
+ * @brief Destructor for the Renderer class.
+ * @details Sets the singleton instance to nullptr upon destruction.
+ */
 Renderer::~Renderer(){
     singleton = nullptr;
 }
