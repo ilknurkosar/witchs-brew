274605596ea12fe8990e4337c6643df30140786b
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 20:55:01 +0300

    Doxygen Comments of GuiButton.cpp

--- src/modules/Core/Node/GuiButton.cpp
+++ src/modules/Core/Node/GuiButton.cpp
@@ -1,5 +1,9 @@
 #include "GuiButton.hpp"
 
+/**
+ * @brief Displays the GUI button using the provided transformation matrix.
+ * @param transform The transformation matrix to apply to the button.
+ */
 void GuiButton::display(Matrix transform){
     bool pressed = raygui::GuiButton(shape, message.c_str());
     if(pressed & (handler != nullptr))
