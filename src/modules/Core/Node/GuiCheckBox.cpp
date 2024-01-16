58a30bdbdb370217af0199662f4556c7cf8927df
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 20:59:47 +0300

    Doxygen Comments of GuiCheckBox.cpp

--- src/modules/Core/Node/GuiCheckBox.cpp
+++ src/modules/Core/Node/GuiCheckBox.cpp
@@ -1,6 +1,9 @@
 #include "GuiButton.hpp"
 #include "GuiCheckBox.hpp"
-
+/**
+ * @brief Displays the GUI checkbox using the provided transformation matrix.
+ * @param transform The transformation matrix to apply to the checkbox.
+ */
 void GuiCheckBox::display(Matrix transform){
     bool old = pressed;
     raygui::GuiCheckBox(shape, message.c_str(), &pressed);
