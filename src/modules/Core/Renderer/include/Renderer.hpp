588b8eda35eca6422838fc0113bb0a423ea9acc1
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 20:51:05 +0300

    Doxygen Comments of Renderer.hpp

--- src/modules/Core/Renderer/include/Renderer.hpp
+++ src/modules/Core/Renderer/include/Renderer.hpp
@@ -4,11 +4,29 @@
 
 // Renderer is a singleton but it has NO mechanisms to enforce it.
 // A standard singleton is indestructible.
+/**
+ * @brief The Renderer class represents a renderer and is derived from the Runnable interface.
+ */
+
 class Renderer : public Runnable{
 public:
     static Renderer *singleton;
 public:
+    /**
+     * @brief Get the singleton instance of the Renderer class.
+     * @return The singleton instance of the Renderer class.
+     */
     static Renderer *getSingleton();
+
+    /**
+     * @brief Constructor for the Renderer class.
+     * @details Initializes the singleton instance.
+     */
     Renderer();
+
+     /**
+     * @brief Destructor for the Renderer class.
+     * @details Sets the singleton instance to nullptr upon destruction.
+     */
     virtual ~Renderer();
 };
