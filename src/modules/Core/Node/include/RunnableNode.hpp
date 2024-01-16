a999d9ff832022dc59b80ac8275cd83a1e92426a
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 21:57:57 +0300

    Doxygen Comment of RunnableNode.hpp

--- src/modules/Core/Node/include/RunnableNode.hpp
+++ src/modules/Core/Node/include/RunnableNode.hpp
@@ -3,8 +3,16 @@
 #include "NodeType.hpp"
 #include "Runnable.hpp"
 
+/**
+ * @brief Represents a node with the ability to be processed as part of a runnable system.
+ * @details This class inherits the NodeType to mark it as processable and can be used in a
+ * hierarchy to organize nodes based on their behavior.
+ */
 class RunnableNode : virtual public NodeType{
 //TODO: figure out a way to make this inherit Runnable instead of Node itselff, without breaking the code
     public:
+        /**
+         * @brief Default constructor for RunnableNode.
+         */
         inline RunnableNode(){setFlag( NodeType::PROCESSABLE);}
 };
