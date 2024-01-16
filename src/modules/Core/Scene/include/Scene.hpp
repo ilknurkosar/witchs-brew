531b633134a675bdbbb56bd10d6629f921356b60
ilknurkosar <87127032+ilknurkosar@users.noreply.github.com>
2024-01-16 19:40:34 +0300

    Doxygen codes of Scene.hpp

--- src/modules/Core/Scene/include/Scene.hpp
+++ src/modules/Core/Scene/include/Scene.hpp
@@ -6,22 +6,61 @@
 #include "VisualNode.hpp"
 #include <vector>
 
+/**
+ * @brief The Scene class represents a scene.
+ * @details This class is derived from the Runnable interface and manages the processes of nodes in the scene.
+ */
 class Scene : public Runnable{
 private:
     static Scene *singleton;
     Node root;
 public:
+      /**
+     * @brief Adds a node to the scene.
+     * @param n The node to be added.
+     */
     static void addNode(Node* n);
+
+     /**
+     * @brief Gets the root node of the scene.
+     * @return The root node of the scene.
+     */
     static Node* getRoot();
+
+     /**
+     * @brief Gets the singleton instance of the scene.
+     * @return The singleton instance of the scene.
+     */
     static inline Scene* getSingleton(){return singleton;}
+
+     /**
+     * @brief Gets a vector containing visual nodes.
+     * @return Vector of visual nodes.
+     */
     static std::vector<Node*> getVisual();
+
+    /**
+     * @brief Gets a vector containing runnable nodes.
+     * @return Vector of runnable nodes.
+     */
     static std::vector<Node*> getRunnable();
     // There is no removal. Nodes delete themselves
     // TODO: except they dont, fix it later
 
+     /**
+     * @brief Scene processing function.
+     * @details Performs the processes of nodes in the scene.
+     */
     virtual void process() override;
 
+      /**
+     * @brief Default constructor for the Scene class.
+     */
     explicit Scene();
+
+    /**
+     * @brief Default destructor for the Scene class.
+     */
     ~Scene();
     friend Node; //so the nodes can remove themselves
 };
